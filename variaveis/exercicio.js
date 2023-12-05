1dfea166b9ed55223c5aca980d91d1ca4d627627
Jônatas Quirino <quirinoj02@gmail.com>
2023-12-05 10:29:54 -0300

    aula (1) - variaveis

---
+++ variaveis/exercicio.js
@@ -0,0 +1,13 @@
+// Declare uma variável com o seu nome
+var nome = "Quirino";
+// Declare uma variável com sua idade
+var idade = 22;
+// Declare uma variável com sua comida favorita e não atribuir valor
+var comidaFavotita;
+// Atribua valor a sua comida favorita
+var comidaFavotita = "frango";
+// Declare 5 variáveis diferentes sem valores
+var time, professor, curso, endereco, rua, cidade;
+// Console
+console.log(nome, idade, comidaFavotita, rua);
+// retunn ---> Quirino 22 frango undefined
---
+++ variaveis/script.js
@@ -0,0 +1,28 @@
+// var, let & const: armazenam valores
+
+var nome = "Jônatas";
+var idade = 28;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+
+// evitam repetições
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+// utiliza-se de vírgula (,) para criar mais uma variável sem repetir a palavra chave.
+
+var sobrenome = "quirino",
+  cidade = "Salvador";
+
+console.log(sobrenome, cidade);
+
+// pode ser declarada como 'sem valor'
+
+var semDefinir;
+console.log(semDefinir);
+// return ---> undefined

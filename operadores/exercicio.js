77bab025e399590206c0fadff17372b6ff54aa6b
Jônatas Quirino <quirinoj02@gmail.com>
2023-12-05 12:04:45 -0300

    aula (3) - operadores

---
+++ operadores/exercicio.js
@@ -0,0 +1,19 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + (5 * 2) / 2 + 20;
+console.log(total);
+// Crie duas expressões que retornam NaN
+var nome = "vic";
+var idade = 23;
+var total = nome / idade;
+console.log(total);
+// Some a String '200' com o número 50 e retorne 250
+var soma = +"200" + 50;
+console.log(soma);
+// Incremente o número 5 e retorne o seu valor incrementado
+var totalNum = 5;
+console.log(++totalNum);
+// Como dividir o peso por 2?
+var numero = +"80" / 2;
+var unidade = "kg ";
+var peso  = numero + unidade; // NaN (Not a Number)
+console.log(peso);
---
+++ operadores/script.js
@@ -0,0 +1,21 @@
+// operadores aritméticos
+var soma = 100 + 50;
+var subtracao = 100 - 50;
+var multiplicacao = 100 * 50;
+var divisao = 100 / 50;
+var expoente = 2 ** 2;
+var modulo = 14 % 3;
+
+console.log(soma);
+
+// operadores aritméticos (strings)
+
+var soma = "100" + 50; // 10050
+var subtracao = "100" - 50; // 50
+var multiplicacao = "100" * 50; // 5000
+var divisao =  "100" / 50; // 2
+var divisaoComCaractere = " Comprei 100" / 50; // NaN
+
+console.log(divisao);
+
+// obs: a ordem importa nas operações, pois é como na matemática

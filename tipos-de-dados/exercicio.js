5de938e980f023bc4ee18b3ae2e088c6ac61301d
Jônatas Quirino <quirinoj02@gmail.com>
2023-12-05 11:06:56 -0300

    aula (2) - tipos de dados

---
+++ tipos-de-dados/exercicio.js
@@ -0,0 +1,18 @@
+// Declare uma variável contendo uma string
+var cidade = "Salvador";
+console.log(typeof cidade);
+// Declare uma variável contendo um número dentro de uma string
+var ano = "2023";
+console.log(typeof idade);
+// Declare uma variável com sua idade
+var idade = 22;
+console.log(typeof idade);
+// Declare duas variáveis, um com seu nome e outra com seu sobrenome e some-as
+var nome = "Jônatas ";
+var sobreNome = "Quirino";
+var nomeCompleto = `${nome}${sobreNome}`;
+console.log(nomeCompleto);
+// Coloque a seguinte frase em uma variável: It's time
+var frase = "It's time";
+// Verifique o tipo de variável que contém o seu nome
+console.log(typeof nome)
---
+++ tipos-de-dados/script.js
@@ -0,0 +1,18 @@
+// string + string
+
+var nome = "Jônatas ";
+var sobrenome = "Quirino";
+var nomeCompleto = nome + sobrenome;
+
+console.log(nomeCompleto);
+
+// string + number
+
+var gols = 1000;
+var frase = "CR7 fez " + gols + " gols";
+console.log(typeof frase);
+
+// template string 
+
+var frase2 = `Messi fez ${gols}`;
+console.log(frase2)
